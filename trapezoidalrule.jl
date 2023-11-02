b1e22f913d376c374b158a5012ebc615c2e05a87
Robin Tietgen <rotietge@students.uni.mainz.de>
2023-11-02 17:31:05 +0100

    All functions implemented

--- trapezoidalrule.jl
+++ trapezoidalrule.jl
@@ -11,17 +11,20 @@ Pkg.instantiate()
 
 
 
-function trapezoidal_rule(function, a,b; N = 100)
+function trapezoidal_rule(func, a,b; N = 100)
   
-    integral = 0
-    dx = (b-a)/N
+    integral = 0 * func(0) #get right dimension for integral
+    dx = (b .- a) / N
+
+    
     x_last = a
     
-    for i in range N
-        x_next = x_last + dx
-        integral = integral + (function(x_last) + function(x_next))/2 * dx
+    for i in range(0,1,N)
+        x_next = x_last .+ dx
+        integral = integral .+ (func(x_last) .+ func(x_next))/2 *dx
         x_last = x_next
     end
+    
     return integral
 
 
@@ -31,9 +34,34 @@ function ftest1(x)
     return cos(200*x)
 end
 
-ftest1(2)
+function ftest2(x)
+    e = exp(2*im*x)
+    return cos(e)/e * im *e
+end
 
+function ftest3(x)
+    return [1, x, x^2, x^3]
+end
 
+function ftest4(x)
+    return x*exp(x)
+end
 
+res1 = -1/200 *(sin(200)-sin(0))
+res2 = 2*pi*im
+res3 = [2,2,8/3,4]
+res4 = trapezoidal_rule(ftest4,-1,2)
+println(trapezoidal_rule(ftest1,0,1))
+println(-1/200 *(sin(200)-sin(0)))
+println()
 
+println(trapezoidal_rule(ftest2,0,2*pi))
+println(2*pi*im)
+println()
 
+println(trapezoidal_rule(ftest3,0,2))
+println([2,2,8/3,4])
+println()
+
+println(trapezoidal_rule(ftest4,-1,2))
+println(exp(2)+2*exp(-1))
